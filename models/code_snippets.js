25cf9190777949a07626a28af312219c3b72d45a
coffeebeans001 <marcel.maynard001@yahoo.com>
2022-11-22 16:32:13 +0000

    nearly all routes completed

--- models/code_snippets.js
+++ models/code_snippets.js
@@ -1,18 +1,67 @@
-// import from ../db/index.js
+// import from ../db/index.js..DONE!
+const { query } = require("../db/index.js");
 
-// query database to return all code snippets 
+// query database to return all code snippets
+async function getSnippets() {
+  const results = await query(`SELECT * FROM snippets`);
+  return results.rows;
+}
 
-// query database to return code snippets by title 
-
+// query database to return code snippets by title
+async function getByTitle(searchTerm) {
+  const results = await query(
+    `SELECT * FROM snippets WHERE snippet_title = $1`,
+    [searchTerm]
+  );
+  return results.rows;
+}
 // query database to return code snippets by snippets_id
+async function getSnippetByID(snippet_id) {
+  const results = await query(`SELECT * FROM snippets WHERE snippet_id = $1`, [
+    snippet_id,
+  ]);
+  return results.rows[0];
+}
 
 // query database to return code snippets by comments_id
+async function getSnippetByComment(comment_id) {
+  const results = await query(`SELECT * FROM snippets WHERE comment_id = $1`, [
+    comment_id,
+  ]);
+  return results.rows[0];
+}
 
 // query database to create code snippet
+async function createSnippet(snippet) {
+  const results = await query(
+    `INSERT INTO snippets (snippet_title, snippet_code, snippet_description) VALUES ($1, $2, $3) RETURNING *`,
+    [snippet.snippet_title, snippet.snippet_code, snippet.snippet_description]
+  );
+  return results.rows[0];
+}
 
-// query database to update code snippet by id 
+// query database to update code snippet by id
+async function updateSnippet(snippet_id, updates) {
+  const results = await query(
+    `UPDATE snippet SET snippet_title = $1, snippet_code = $2, snippet_description = $3 WHERE snippet_id = $4 RETURNING *`,
+    [
+      updates.snippet_title,
+      updates.snippet_code,
+      updates.snippet_description,
+      snippet_id,
+    ]
+  );
+  return results.rows[0];
+}
 
 // query database to delete customer by id
 
 // export all the functions
-
+module.exports = {
+  getSnippets,
+  getByTitle,
+  getSnippetByID,
+  getSnippetByComment,
+  createSnippet,
+  updateSnippet,
+};
--- routes/code_snippets.js
+++ routes/code_snippets.js
@@ -5,26 +5,33 @@ const codeRouter = express.Router();
 
 // Importing routers to our models
 const {
+  getSnippets,
   getByTitle,
   getSnippetByID,
+  getSnippetByComment,
   createSnippet,
   updateSnippet,
   deleteSnippet,
 } = require("../models/code_snippets.js");
 
-
 // Get code snippets by TITLE
 codeRouter.get("/", async (req, res) => {
- if (req.query.search != undefined) {
-    //  const result = await getByTitle(req.query.search);
-     const result = "test"
+  if (req.query.search != undefined) {
+    const result = await getByTitle(req.query.search);
+    // const result = "test";
     res.json({
       success: true,
       payload: result,
     });
   }
+  //else {
+  //const result = await getSnippets();
+  //res.json({
+  //success: true,
+  //payload: result,
+  // });
 });
-
+// get code snippets by comment id
 // Get code snippets by ID
 
 codeRouter.get("/:id", async (req, res) => {
@@ -40,7 +47,7 @@ codeRouter.get("/:id", async (req, res) => {
 codeRouter.post("/", async (req, res) => {
   const result = await createSnippet(req.body);
   res.json({
-    success: true, 
+    success: true,
     payload: result,
   });
 });
@@ -50,9 +57,9 @@ codeRouter.post("/", async (req, res) => {
 codeRouter.patch("/id", async (req, res) => {
   const result = await updateSnippet(req.params.id, req.body);
   res.json({
-    success: true, 
+    success: true,
     payload: result,
-  })
+  });
 });
 
 // Delete code snippet by ID
@@ -61,9 +68,9 @@ codeRouter.delete("/id", async (req, res) => {
   const result = await deleteSnippet(req.params.id);
   res.json({
     success: true,
-    payload: result
-  })
-})
+    payload: result,
+  });
+});
 
 // Export routers..DONE!
 module.exports = codeRouter;

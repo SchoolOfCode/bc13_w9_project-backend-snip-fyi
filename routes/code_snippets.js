e8b539428ba03b784efd03a461caa40250e7543a
fabbiha <sfabbiha@gmail.com>
2022-11-22 13:22:53 +0000

    completed post, patch and delete routers

--- routes/code_snippets.js
+++ routes/code_snippets.js
@@ -1,11 +1,18 @@
 // Import express..DONE!
 // Import routers..DONE!
-// Export routers..DONE!
-// getAllByTitle
-
 const express = require("express");
 const codeRouter = express.Router();
 
+// Importing routers to our models
+const {
+  getByTitle,
+  createSnippet,
+  updateSnippet,
+  deleteSnippet,
+} = require("../models/code_snippets.js");
+
+
+// Get code snippets by TITLE
 codeRouter.get("/", async (req, res) => {
  if (req.query.search != undefined) {
      const result = await getByTitle(req.query.search);
@@ -17,4 +24,37 @@ codeRouter.get("/", async (req, res) => {
   }
 });
 
+// Get code snippets by ID
+
+// POST code snippet
+
+codeRouter.post("/", async (req, res) => {
+  const result = await createSnippet(req.body);
+  res.json({
+    success: true, 
+    payload: result,
+  });
+});
+
+// PATCH code snippet by ID
+
+codeRouter.patch("/id", async (req, res) => {
+  const result = await updateSnippet(req.params.id, req.body);
+  res.json({
+    success: true, 
+    payload: result,
+  })
+});
+
+// Delete code snippet by ID
+
+codeRouter.delete("/id", async (req, res) => {
+  const result = await deleteSnippet(req.params.id);
+  res.json({
+    success: true,
+    payload: result
+  })
+})
+
+// Export routers..DONE!
 module.exports = codeRouter;

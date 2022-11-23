901f12689fc15d1078fbc9b2bfb1e2ae8337c3ca
bennyfreemantle <bennyfreemantle@protonmail.com>
2022-11-23 18:47:08 +0000

    Creating a new comment and find comment by snippet id

--- models/comments.js
+++ models/comments.js
@@ -4,13 +4,9 @@ const { query } = require("../db/index.js");
 
 // query comment table to return all comments by id
 
-async function getComments(id) {
-  const results = await query(
-    `SELECT *
-  FROM comments WHERE comment_id = $1`,
-    [id]
-  );
-  return results.rows[0];
+async function getComments() {
+  const results = await query(`SELECT * FROM comments;`);
+  return results.rows;
 }
 
 //Query comment table to delete comment by ID
@@ -21,5 +17,33 @@ async function deleteComment(id) {
   return results.rows[0];
 }
 
+// Create new comment
+async function createComment({
+  snippet_id,
+  comment_content,
+  comment_author,
+  comment_date_create,
+}) {
+  const result = await query(
+    `INSERT INTO comments (snippet_id, comment_content, comment_author, comment_date_create) VALUES ($1, $2, $3, $4) RETURNING *;`,
+    [snippet_id, comment_content, comment_author, comment_date_create]
+  );
+  return result.rows[0];
+}
+
+async function getCommentsBySnippetId(id) {
+  const results = await query(
+    `SELECT *
+  FROM comments WHERE snippet_id = $1`,
+    [id]
+  );
+  return results.rows;
+}
+
 // export it
-module.exports = { getComments, deleteComment };
+module.exports = {
+  getComments,
+  getCommentsBySnippetId,
+  deleteComment,
+  createComment,
+};
--- routes/comments.js
+++ routes/comments.js
@@ -8,14 +8,38 @@ const commentRouter = express.Router();
 
 // importing our models from comments.js
 
-const { getComments, deleteComment } = require("../models/comments.js");
+const {
+  getComments,
+  getCommentsBySnippetId,
+  createComment,
+  deleteComment,
+} = require("../models/comments.js");
 
 // get comments by comment_id
 
-commentRouter.get("/:id", async (req, res) => {
+commentRouter.get("/", async (req, res) => {
   res.json({
     success: true,
     payload: await getComments(req.params.id),
+  });
+});
+
+// get all comments with the given snippet_id
+commentRouter.get("/:id", async (req, res) => {
+  const result = await getCommentsBySnippetId(req.params.id);
+  res.json({
+    success: true,
+    payload: result,
+  });
+});
+
+// Create new comment
+commentRouter.post("/", async (req, res) => {
+  const payload = req.body;
+  const result = await createComment(payload);
+  res.json({
+    success: true,
+    payload: result,
   });
 });
 

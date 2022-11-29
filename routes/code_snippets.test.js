0d54116289209cfb7f2787ce7884e18525889cac
fabbiha <sfabbiha@gmail.com>
2022-11-29 11:00:15 +0000

    added try catch() to code snippet and comments test

--- routes/code_snippets.test.js
+++ routes/code_snippets.test.js
@@ -4,12 +4,16 @@ import app from "../app";
 
 
 test("Getting code snippets, checking status code and response body", async () => {
-  const response = await supertest(app).get("/api/codesnippet");
-  expect(response.status).toBe(200);
-  expect(response.body).toStrictEqual({
-    success: true,
-    payload: expect.any(Array),
-  });
+  try {
+    const response = await supertest(app).get("/api/codesnippet");
+    expect(response.status).toBe(200);
+    expect(response.body).toStrictEqual({
+      success: true,
+      payload: expect.any(Array),
+    });
+  } catch (error) {
+    console.log(error)
+  }
 });
 
 
@@ -18,7 +22,9 @@ test("POST /api/codesnippet", async () => {
   const snippet_code = "if (hour < 18) {\r\n  greeting = \"Good day\";\r\n}"; // needs to be valid JS
   const snippet_description = "dummy dsecription";
   const snippet_date_create = "2022-11-24"
-  const response = await supertest(app)
+  
+  try {
+    const response = await supertest(app)
     .post("/api/codesnippet")
 
     .send({
@@ -28,34 +34,41 @@ test("POST /api/codesnippet", async () => {
       snippet_date_create: snippet_date_create,
     });
 
-  expect(response.status).toBe(200);
+    expect(response.status).toBe(200);
 
-  expect(response.body).toStrictEqual({
-    success: true,
-    payload: {
+     expect(response.body).toStrictEqual({
+      success: true,
+      payload: {
       snippet_id: expect.any(Number),
       snippet_title: expect.any(String),
       snippet_code: expect.any(String),
       snippet_description: expect.any(String),
       snippet_date_create: "2022-11-24", //postgres date format is YYYY-MM-DD
-    },
-  });
+      },
+  })
+  } catch (error) {
+    console.log(error)
+  }
 });
 
 test("DELETE /api/codesnippet", async () => {
-  const idToDelete = 70;
-  const response = await supertest(app).delete("/api/codesnippet/70");
-  expect(response.status).toBe(200);
-
-  expect(response.body).toStrictEqual({
-    success: true,
-    payload: {
-      snippet_id: idToDelete,
-      snippet_title: "while loop",
-      snippet_code: "if (hour < 18) {\r\n  greeting = \"Good day\";\r\n}",
-      snippet_description: expect.any(String),
-      snippet_date_create: "2022-11-24"
-    },
-  })
-})
+  try {
+    const idToDelete = 70;
+    const response = await supertest(app).delete("/api/codesnippet/70");
+    expect(response.status).toBe(200);
+  
+    expect(response.body).toStrictEqual({
+      success: true,
+      payload: {
+        snippet_id: idToDelete,
+        snippet_title: "while loop",
+        snippet_code: "if (hour < 18) {\r\n  greeting = \"Good day\";\r\n}",
+        snippet_description: expect.any(String),
+        snippet_date_create: "2022-11-24"
+      },
+    })
+  } catch (error) {
+    console.log(error)
+  }
+});
 
--- routes/comments.test.js
+++ routes/comments.test.js
@@ -4,45 +4,55 @@ import app from "../app";
 
 
 test("GET /api/codecomment", async () => {
-  const response = await supertest(app).get("/api/codecomment");
 
-  expect(response.status).toBe(200);
+  try {
+    const response = await supertest(app).get("/api/codecomment");
 
-  expect(response.body).toStrictEqual({
-    success: true,
-    payload: expect.any(Array),
-  });
+    expect(response.status).toBe(200);
+  
+    expect(response.body).toStrictEqual({
+      success: true,
+      payload: expect.any(Array),
+    });    
+  } catch (error) {
+    console.log(error)
+  }
 });
 
 
 test("POST /api/codecomment", async () => {
-  const snippet_id = "71";
-  const comment_content = "Test content";
-  const comment_author = "Marcel and Fabbi";
-  const comment_date_create = "24-11-2022";
 
-  const response = await supertest(app)
-    .post("/api/codecomment")
-
-    .send({
-      snippet_id: snippet_id,
-      comment_content: comment_content,
-      comment_author: comment_author,
-      comment_date_create: comment_date_create,
+  try {
+    const snippet_id = "71";
+    const comment_content = "Test content";
+    const comment_author = "Marcel and Fabbi";
+    const comment_date_create = "24-11-2022";
+  
+    const response = await supertest(app)
+      .post("/api/codecomment")
+  
+      .send({
+        snippet_id: snippet_id,
+        comment_content: comment_content,
+        comment_author: comment_author,
+        comment_date_create: comment_date_create,
+      });
+  
+    expect(response.status).toBe(200);
+  
+    expect(response.body).toStrictEqual({
+      success: true,
+      payload: {
+        comment_id: expect.any(Number),
+        snippet_id: expect.any(Number),
+        comment_content: expect.any(String),
+        comment_author: expect.any(String),
+        comment_date_create: "24-11-2022",
+      },
     });
-
-  expect(response.status).toBe(200);
-
-  expect(response.body).toStrictEqual({
-    success: true,
-    payload: {
-      comment_id: expect.any(Number),
-      snippet_id: expect.any(Number),
-      comment_content: expect.any(String),
-      comment_author: expect.any(String),
-      comment_date_create: "24-11-2022",
-    },
-  });
+  } catch (error) {
+    console.log(error)
+  }
 });
 
 // commented out because it's not working, will come back to later to fix.
